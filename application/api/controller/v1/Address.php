394efed8b25c2e4f462912d65ca91082e04c1b16
Zhao-666 <2268699370@qq.com>
2017-12-25 22:45:40 +0800

    9-12 不要明文传送用户的uid。只需要传递token再从缓存中获取uid更安全

---
+++ application/api/controller/v1/Address.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Next
+ * Date: 2017/12/25
+ * Time: 22:29
+ */
+
+namespace app\api\controller\v1;
+
+
+class Address
+{
+    public function createOrUpdateAddress()
+    {
+
+    }
+}
---
+++ application/api/validate/AddressNew.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Next
+ * Date: 2017/12/25
+ * Time: 22:31
+ */
+
+namespace app\api\validate;
+
+
+class AddressNew extends BaseValidate
+{
+    protected $rule = [
+        'name' => 'require|isNotEmpty',
+        'mobile' => 'require|isMobile',
+        'province' => 'require|isNotEmpty',
+        'city' => 'require|isNotEmpty',
+        'country' => 'require|isNotEmpty',
+        'detail' => 'require|isNotEmpty',
+    ];
+}
