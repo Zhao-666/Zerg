943ca1d57530d7c6d992ad13f1b6f1d9ed16401f
Zhao-666 <2268699370@qq.com>
2017-12-25 23:14:50 +0800

    9-13 增加了地址属性validate。封装获取Token的方法在Service层

--- application/api/controller/v1/Address.php
+++ application/api/controller/v1/Address.php
@@ -9,10 +9,15 @@
 namespace app\api\controller\v1;
 
 
+use app\api\validate\AddressNew;
+use app\api\service\Token as TokenService;
+
 class Address
 {
     public function createOrUpdateAddress()
     {
-
+        (new AddressNew())->goCheck();
+        $uid = TokenService::getCurrentUid();
+        
     }
 }
--- application/api/service/Token.php
+++ application/api/service/Token.php
@@ -9,12 +9,40 @@
 namespace app\api\service;
 
 
+use app\lib\exception\TokenException;
+use think\Exception;
+
 class Token
 {
-    public static function generateToken(){
+    public static function generateToken()
+    {
         $randChars = getRandChar(32);
         $timestamp = input('server.REQUEST_TIME_FLOAT');
         $salt = config('secure.token_salt');
-        return md5($randChars.$timestamp.$salt);
+        return md5($randChars . $timestamp . $salt);
+    }
+
+    public static function getCurrentTokenVar($key)
+    {
+        $token = request()->header('token');
+        $vars = cache($token);
+        if (empty($vars)) {
+            throw new TokenException();
+        } else {
+            if (!is_array($vars)) {
+                $vars = json_decode($vars);
+            }
+            if (array_key_exists($key, $vars)) {
+                return $vars[$key];
+            } else {
+                throw new Exception('尝试获取的Token变量并不存在');
+            }
+        }
+    }
+
+    public static function getCurrentUid()
+    {
+        $uid = self::getCurrentTokenVar('uid');
+        return $uid;
     }
 }
